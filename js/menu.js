98043ad7f8d5817db7e97c7e15b7f89ce837ce8e
fraguival <73153952+fraguival@users.noreply.github.com>
2021-12-16 17:02:31 -0700

    a

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+let abrirMenu = document.getElementById('abrirmenu');
+let menu = document.getElementById('menu');
+
+   abrirMenu.addEventListener('click', function() {
+       'use strict';
+
+       menu.classList.toggle('mostrar');
+   });
